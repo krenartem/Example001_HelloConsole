082e1bfaec561d29e4dd2efdf13af8214d7c9986
krenartem <krenartem@yandex.ru>
2022-08-07 18:24:47 +0300

    add random to Example 003 Sum

--- Example001_HelloConsole/Example003_Sum/Program.cs
+++ Example001_HelloConsole/Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 32;
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 10); // случ. число от 0 до 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(5, 20);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(numberA + numberB);
